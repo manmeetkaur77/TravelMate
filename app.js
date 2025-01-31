3296a4d62e5a84283d9e64429415899f28f1e7e8
manmeetkaur77 <manmeetkaur1629@gmail.com>
2025-01-31 23:57:38 +0530

    configure dev dependencies

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 
 const express = require("express");
-const morgan=require("morgan");
-const tourRouter=require("./Routes/tourRoutes.js");
-const userRouter=require("./Routes/userRoutes.js");
+const morgan = require("morgan");
+const tourRouter = require("./Routes/tourRoutes.js");
+const userRouter = require("./Routes/userRoutes.js");
 
-const app = express();
-app.use(morgan('dev'));
+// ✅ Define app first
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+const app = express(); 
+
+app.use(morgan("dev"));
+
 
 // Middleware to parse JSON in requests
 app.use(express.json());
 
-
-
 //-------------------------------------------------ROUTES-------------------------------------------------
-app.use("/api/v1/tours",tourRouter);
-app.use("/api/v1/users",userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-module.exports=app;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //-------------------------------------------------SERVER SETUP-------------------------------------------------
-const dotenv=require("dotenv");
-const app=require("./app.js")
-const port = 3000;
-dotenv.config({path: "/Users/manmeetkaur/Desktop/Big Folder/TravelMate/congif.env"});
-console.log(process.env);
+const dotenv = require("dotenv");
+dotenv.config({path:'./config.env'});
+
+const app = require("./app.js");
+
+
+const port = process.env.PORT || 3000;
+console.log(process.env)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
